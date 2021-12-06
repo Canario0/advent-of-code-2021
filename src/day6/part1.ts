6619ec28f7f8d81eb189a7f9df7ce13502932d54
Pablo Renero Balgañón <prenero4bc@gamil.com>
2021-12-06 18:51:28 +0100

    feat(day6): add part1 solution

---
+++ src/day6/part1.ts
@@ -0,0 +1,42 @@
+class FishGroup {
+  timer = 0;
+  readonly fishCount: number;
+
+  constructor(timer: number, fishCount: number) {
+    this.timer = timer;
+    this.fishCount = fishCount;
+  }
+
+  updateTimer(): FishGroup | null {
+    if (!this.timer) {
+      this.timer = 6;
+      return this.giveBirth()
+    }
+    this.timer--;
+    return null;
+  }
+
+  giveBirth(): FishGroup {
+    return new FishGroup(8, this.fishCount);
+  }
+}
+
+export function solve(days: number, fishes: number[]): number {
+  let fishGroups = Array.from(
+    fishes
+      .sort()
+      .reduce((acc, val) => {
+        acc.set(val, (acc.get(val) ?? 0) + 1);
+        return acc;
+      }, new Map<number, number>())
+      .entries()
+  ).map(([timer, fishCount]) => new FishGroup(timer, fishCount));
+  for (let day = 0; day < days; day++) {
+    fishGroups = fishGroups.concat(
+      fishGroups
+        .map((fishGroup) => fishGroup.updateTimer())
+        .filter((fishGroups) => fishGroups != null) as FishGroup[]
+    );
+  }
+  return fishGroups.map((fishGroup) => fishGroup.fishCount).reduce((acc, val) => acc +  val, 0);
+}
